d8ee2d12f3d406fb70e48cf8bc949a18959d8559
DarkIceXD <15961071+DarkIceXD@users.noreply.github.com>
2021-08-28 13:55:54 +0200

    warzone 1.41.2 update

--- DarkTool Warzone/game/offsets.h
+++ DarkTool Warzone/game/offsets.h
@@ -1,20 +1,20 @@
 #pragma once
 
 namespace offsets {
-	constexpr auto refdef = 0x17CADC30;
-	constexpr auto name_array = 0x17CB9258;
+	constexpr auto refdef = 0x17932CB0;
+	constexpr auto name_array = 0x1793E2D8;
 	constexpr auto name_array_pos = 0x4C70;
-	constexpr auto camera_base = 0x14D2BE70;
+	constexpr auto camera_base = 0x149B0E70;
 	constexpr auto camera_pos = 0x1D8;
-	constexpr auto local_index = 0x6E10;
+	constexpr auto local_index = 0x31DC0;
 	constexpr auto local_index_pos = 0x1FC;
 	namespace player {
-		constexpr auto size = 0x3AC8;
-		constexpr auto valid = 0x684;
-		constexpr auto pos = 0x8B0;
-		constexpr auto team = 0x1258;
-		constexpr auto stance = 0x944;
-		constexpr auto dead_1 = 0x67C;
-		constexpr auto dead_2 = 0x1178;
+		constexpr auto size = 0x3AE0;
+		constexpr auto valid = 0x504;
+		constexpr auto pos = 0x100;
+		constexpr auto team = 0x4D0;
+		constexpr auto stance = 0x98C;
+		constexpr auto dead_1 = 0x27C;
+		constexpr auto dead_2 = 0x570;
 	}
 }
